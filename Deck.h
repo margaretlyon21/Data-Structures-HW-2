8af466d9ec4a8be50414e3da80645eff17150b4f
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-03-06 14:34:10 -0500

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,1 +1,20 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include "Node.h"
+
+class Deck {
+private:
+    Node* front;
+    Node* rear;
+    int size;
+public:
+    Deck();
+    bool isEmpty();
+    void enqueue(int card);
+    int dequeue();
+    int getSize();
+};
+
+#endif
 
